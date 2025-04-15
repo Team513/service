f8a360fe49b96535f4e85c76b6b027f95177c2e0
Timotei Andrei <timiandrei223@gmail.com>
2025-04-15 07:29:32 +0000

    Changed localhost

--- src/test/java/ro/unibuc/hello/controller/RobotControllerIntegrationTest.java
+++ src/test/java/ro/unibuc/hello/controller/RobotControllerIntegrationTest.java
@@ -41,7 +41,7 @@ public class RobotControllerIntegrationTest {
 
     @DynamicPropertySource
     static void setProperties(DynamicPropertyRegistry registry) {
-        final String MONGO_URL = "mongodb://localhost:";
+        final String MONGO_URL = "mongodb://host.docker.internal:";
         final String PORT = String.valueOf(mongoDBContainer.getMappedPort(27017));
         registry.add("mongodb.connection.url", () -> MONGO_URL + PORT);
     }
